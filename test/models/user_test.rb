8ab2519331a319157ee22ce277a0f7022e2fd29d
shoki-0303 <shoki522.33333@gmail.com>
2018-09-23 13:44:48 +0900

    add test should be valid?

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,4 +5,8 @@ class UserTest < ActiveSupport::TestCase
   def setup
     @user = User.new(name: 'test_user', email: 'test@example.com')
   end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
 end
